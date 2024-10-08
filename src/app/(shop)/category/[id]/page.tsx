35d74c058fa7d2dfacd8f03b4cbf55d639d56c20
cfcamilafabre <camilafabre98 @gmail.com>
2024-10-08 08:42:31 -0300

    categorys, interfaces, filtros

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,48 @@
-import { notFound } from "next/navigation";
+import { ProductGrid, } from '@/components';
+import Title from '@/components/ui/Title';
+import { Category } from '@/interfaces';
+import { initialData } from '@/seed/seed';
+import { notFound } from 'next/navigation';
+
+const seedProducts = initialData.products;
+
 
 interface Props {
   params: {
-    id: string;
-  };
+    id: Category;
+  }
 }
 
-export default function ({ params }: Props) {
-  const { id } = params;
 
-  if (id === "kids") {
-    notFound();
+export default function({ params }: Props) {
+
+  const { id } = params;
+  const products = seedProducts.filter( product => product.gender === id );
+
+  const labels: Record<Category, string>  = {
+    'men': 'para hombres',
+    'women': 'para mujeres',
+    'kid': 'para niños',
+    'unisex': 'para todos'
   }
 
+  // if ( id === 'kids' ) {
+  //   notFound();
+  // }
+
+
   return (
-    <div>
-      <h1>Category Pageeee {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`Artículos de ${ labels[id] }`}
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+
+      <ProductGrid 
+        products={ products }
+      />
+      
+    </>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+
+
+
+
+export * from './products/product-grid/ProductGrid';
+export * from './products/product-grid/ProductGridItem';
+
+export * from './ui/PageNotFound';
+export * from './ui/sidebar/Sidebar';
+export * from './ui/Title';
+export * from './ui/top-menu/TopMenu';
